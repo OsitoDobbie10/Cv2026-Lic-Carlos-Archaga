390395839e36e09552d89b7d91a4f35a28062a5e
OsitoDobbie10SIG <74443397+OsitoDobbie10@users.noreply.github.com>
2026-05-07 17:52:05 -0600

    data

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -2,10 +2,11 @@ import Image from "next/image"
 import TransitionComponent from "./TransitionComponent"
 import Dev from "@/public/programacion.png"
 const Avatar = () => {
+
   return (
     <TransitionComponent position='bottom' className="bottom-0 right-0 hidden md:inline-block md:absolute">
       <Image src={Dev} alt="ImagenDevs" width={300} height={300} className="w-full h-full"/>
-    </TransitionComponent>
+    </TransitionComponent> 
   )
 }
 
