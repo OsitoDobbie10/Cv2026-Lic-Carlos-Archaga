2b585828ad5296896f3bb72e693dc96cb156aa74
OsitoDobbie10SIG <74443397+OsitoDobbie10@users.noreply.github.com>
2026-05-07 20:44:59 -0600

    Cambio LineLife
    Updated padding and font size for better layout.

--- components/LineLife.tsx
+++ components/LineLife.tsx
@@ -7,12 +7,12 @@ const LineLife = () => {
     <div className="-my-6">
     {
         dataAboutPage.map(({id,title,subtitle,description,date})=>{
-           return  <div key={id} className="relative py-6 pl-8 sm:pl-32 group">
-                   <h3 className="mb-1 text-2xl font-bold sm:mb-0">
+           return  <div key={id} className="relative py-7 pl-8 sm:pl-32 group">
+                   <h3 className="mb-1 text-xl font-bold sm:mb-0">
                    {title}
                    </h3>
                    <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-indigo-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">                
-                   <time className="sm:absolute left-0 traslate-y-0.5 inline-flex items-center justify-center text-sm font-semibold uppercase w-20 h-8 mb-3 sm:mb-0">
+                   <time className="relative sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-sm font-semibold uppercase w-20 h-8 mb-3 sm:mb-0" >
                     {date}
                    </time>
                    <p className="text-xl font-bold text-gray-400">
