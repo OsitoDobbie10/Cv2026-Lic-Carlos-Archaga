bb658d60747e08a467cbfaeb72cd091038636d3e
OsitoDobbie10SIG <74443397+OsitoDobbie10@users.noreply.github.com>
2026-05-07 22:44:37 -0600

    Update Porfoliobox to display text instead of image
    Replaced image display with text and updated link labels.

--- components/Porfoliobox.tsx
+++ components/Porfoliobox.tsx
@@ -17,13 +17,10 @@ const Porfoliobox = (props:PorfolioBox) => {
   return (
     <div className='p-4 border border-white'>
     <h3 className='mb-4 text-xl'>{title}</h3>
-    <Image src={image} alt={title} width={300} height={300} className='w-full md:w-[300px] h-auto'/> 
+    <div className="mb-4 text-6xl text-secondary text-center">{image}</div>
     <div className="flex gap-5 mt-5" >
-    <Link href={urlGithub} target='_blank' className='p-2 transition duration-150 rounded-lg bg-slate-500 hover:bg-slate-500/50'>
-     Github
-    </Link> 
     <Link href={urlDemo} target='_blank' className='p-2 transition duration-150 rounded-lg bg-secondary hover:bg-secondary/50'>
-     Url Demo
+     Mirar
     </Link> 
     </div>
     </div>
