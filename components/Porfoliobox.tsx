b1fca7e745231d42a8d9b0a9d9acd638cb82fe9a
OsitoDobbie10SIG <74443397+OsitoDobbie10@users.noreply.github.com>
2026-05-07 23:04:15 -0600

    Replace image placeholder with Image component

--- components/Porfoliobox.tsx
+++ components/Porfoliobox.tsx
@@ -17,10 +17,13 @@ const Porfoliobox = (props:PorfolioBox) => {
   return (
     <div className='p-4 border border-white'>
     <h3 className='mb-4 text-xl'>{title}</h3>
-    <div className="mb-4 text-6xl text-secondary text-center">{image}</div>
+    <Image src={image} alt={title} width={300} height={300} className='w-full md:w-[300px] h-auto'/> 
     <div className="flex gap-5 mt-5" >
+    <Link href={urlGithub} target='_blank' className='p-2 transition duration-150 rounded-lg bg-slate-500 hover:bg-slate-500/50'>
+     Github
+    </Link> 
     <Link href={urlDemo} target='_blank' className='p-2 transition duration-150 rounded-lg bg-secondary hover:bg-secondary/50'>
-     Mirar
+     Url Demo
     </Link> 
     </div>
     </div>
