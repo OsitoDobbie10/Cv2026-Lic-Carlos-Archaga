965bce21ca0dedbe09d109f0fb36168476a4a5d5
OsitoDobbie10SIG <74443397+OsitoDobbie10@users.noreply.github.com>
2026-05-07 23:13:32 -0600

    Update GitHub and demo URLs in data.tsx 2

--- data.tsx
+++ data.tsx
@@ -213,15 +213,15 @@ export const dataPortfolio = [
         id:4,
         title:"Dashboard AMDC para visualizar uso de graficos con content HTML",
         image:"https://insightsoftware.com/wp-content/uploads/2018/05/microsoft-power-bi-maps-tutorial-1170x400.jpg",
-        urlGithub:"https://app.powerbi.com/view?r=eyJrIjoiNjU5YmM0YTEtNWU3ZS00NGI0LTgzYjgtYTRjNWJhMDM3ZTY2IiwidCI6ImMxYjIyNzEzLWUwMTUtNDQ5Ny04YWY3LWFjNzY4MDNmZGFjNSIsImMiOjR9",
-        urlDemo:"https://vistapasebatch.netlify.app/"
+        urlGithub:"https://github.com/OsitoDobbie10/MoviesYT_APP_React",
+        urlDemo:"https://app.powerbi.com/view?r=eyJrIjoiNjU5YmM0YTEtNWU3ZS00NGI0LTgzYjgtYTRjNWJhMDM3ZTY2IiwidCI6ImMxYjIyNzEzLWUwMTUtNDQ5Ny04YWY3LWFjNzY4MDNmZGFjNSIsImMiOjR9"
     },
     {
         id:5,
         title:"Dashboard para recursos humanos vista",
         image:"https://cdn-icons-png.flaticon.com/512/9670/9670755.png",
-        urlGithub:"https://app.powerbi.com/view?r=eyJrIjoiNzM1ODUwZDItYjNmYS00MTIwLTlmMjAtZjFmMzkyYWFjYzE4IiwidCI6ImMxYjIyNzEzLWUwMTUtNDQ5Ny04YWY3LWFjNzY4MDNmZGFjNSIsImMiOjR9",
-        urlDemo:"https://vistapasebatch.netlify.app/"
+        urlGithub:"https://github.com/OsitoDobbie10/MoviesYT_APP_React",
+        urlDemo:"https://app.powerbi.com/view?r=eyJrIjoiNzM1ODUwZDItYjNmYS00MTIwLTlmMjAtZjFmMzkyYWFjYzE4IiwidCI6ImMxYjIyNzEzLWUwMTUtNDQ5Ny04YWY3LWFjNzY4MDNmZGFjNSIsImMiOjR9"
     }
 ];
 
