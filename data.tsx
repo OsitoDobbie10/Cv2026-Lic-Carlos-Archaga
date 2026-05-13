289b1096d51ddced81174f720fad4662d31c8708
OsitoDobbie10SIG <74443397+OsitoDobbie10@users.noreply.github.com>
2026-05-12 19:55:38 -0600

    Update data.tsx

--- data.tsx
+++ data.tsx
@@ -212,14 +212,14 @@ export const dataPortfolio = [
      {
         id:4,
         title:"Dashboard AMDC para visualizar uso de graficos con content HTML",
-        image:"/Vista Para CV 2.png",
+        image:"/Vista Para CV 1.png",
         urlGithub:"https://github.com/OsitoDobbie10/MoviesYT_APP_React",
         urlDemo:"https://app.powerbi.com/view?r=eyJrIjoiNjU5YmM0YTEtNWU3ZS00NGI0LTgzYjgtYTRjNWJhMDM3ZTY2IiwidCI6ImMxYjIyNzEzLWUwMTUtNDQ5Ny04YWY3LWFjNzY4MDNmZGFjNSIsImMiOjR9"
     },
     {
         id:5,
         title:"Dashboard para recursos humanos vista",
-        image:"/Vista Para CV 1.png",
+        image:"/Vista Para CV 2.png",
         urlGithub:"https://github.com/OsitoDobbie10/MoviesYT_APP_React",
         urlDemo:"https://app.powerbi.com/view?r=eyJrIjoiNzM1ODUwZDItYjNmYS00MTIwLTlmMjAtZjFmMzkyYWFjYzE4IiwidCI6ImMxYjIyNzEzLWUwMTUtNDQ5Ny04YWY3LWFjNzY4MDNmZGFjNSIsImMiOjR9"
     }
