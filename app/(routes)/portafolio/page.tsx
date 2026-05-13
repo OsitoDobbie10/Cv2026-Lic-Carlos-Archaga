c9c2fab3ebdfde160fdef71fe1a43bf647d89c41
OsitoDobbie10SIG <74443397+OsitoDobbie10@users.noreply.github.com>
2026-05-12 20:48:33 -0600

    Update header text for portfolio page

--- app/(routes)/portafolio/page.tsx
+++ app/(routes)/portafolio/page.tsx
@@ -15,7 +15,7 @@ const page = () => {
     <AvatarPortafolio/>
     <CircleImage/>
     <TransitionComponent position='bottom' className="flex flex-col justify-center' h-full">
-    <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-5">Mis ultimos <span className='text-secondary font-bold'>trabajos realizados</span></h1> 
+    <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-5">Proyectos personales realizados <span className='text-secondary font-bold'>enfocados a ciencia de datos y desarrollo web</span></h1> 
     <div className="relative z-10 grid max-w-5xl gap-6 mx-auto mt-4 md:grid-cols-4 sm:grid-cols-2 ">
     {
         dataPortfolio.map((data)=>{
